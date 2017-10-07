2c2d8f2a6fea2e4061307019392da95282e06347
StardustGogeta <stardustgogeta0@gmail.com>
2017-10-07 17:08:31 -0400

    Fixing Chess.com scraping
    This fixes issues with YQL deprecating the HTML scraper and Chess.com deprecating the old website design.

--- JS/common.js
+++ JS/common.js
@@ -1,8 +1,9 @@
 function chess(d) {
-	document.getElementById('blitz').innerHTML = d[0];
-	document.getElementById('bullet').innerHTML = d[1];
-	document.getElementById('standard').innerHTML = d[2];
-	document.getElementById('tactics').innerHTML = d[5];
+	stats = d.match(/\d+/g);
+	document.getElementById('bullet').innerHTML = stats[1];
+	document.getElementById('blitz').innerHTML = stats[2];
+	document.getElementById('rapid').innerHTML = stats[5];
+	document.getElementById('tactics').innerHTML = stats[0];
 };
 		
 function fetchData() {
@@ -10,8 +11,8 @@ function fetchData() {
 		document.getElementById('karma1').innerHTML = body.data.link_karma;
 		document.getElementById('karma2').innerHTML = body.data.comment_karma;
 	});
-	fetch("https://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.chess.com%2Fmember%2FStardustGogeta'%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22right%22%5D'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=chess")
-	.then(r=>r.text()).then(body => chess(JSON.parse(body.substr(10,140)+'}').query.results.div));
+	fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D%27https%3A%2F%2Fwww.chess.com%2Fstats%2Flive%2Fstardustgogeta%27%20and%20xpath%3D%22%2F%2Fspan%5B%40class%3D%27user-rating%27%5D%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+	.then(r=>r.text()).then(body => chess(JSON.parse(body).query.results.result));
 	fetch("https://api.stackexchange.com/2.2/users/5732397?site=stackoverflow").then(r=>r.json())
 	.then(body => document.getElementById('SOrep').innerHTML = body.items[0].reputation);
 }
