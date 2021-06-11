0e43603e78f2e3622d0dd10cd974bdd31a3cfedb
StardustGogeta <stardustgogeta0@gmail.com>
2021-06-11 15:34:14 -0400

    More math things

--- cmsc250/math.js
+++ cmsc250/math.js
@@ -1,4 +1,4 @@
-set = {delimiters:[{left:"`",right:"`",display:false}]};
+set = {delimiters:[{left:"`",right:"`",display:false}, {left:"$$",right:"$$",display:true}]};
 renderMathInElement(document.getElementsByClassName('container')[0],set);
 
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
