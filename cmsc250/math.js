ce17cb0b9e857873348165562d0cac0f5c955f02
StardustGogeta <stardustgogeta0@gmail.com>
2025-02-21 15:35:43 -0500

    Remove old content

--- cmsc250/math.js
+++
@@ -1,7 +0,0 @@
-set = {delimiters:[{left:"`",right:"`",display:false}, {left:"$$",right:"$$",display:true}]};
-renderMathInElement(document.getElementsByClassName('container')[0],set);
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
